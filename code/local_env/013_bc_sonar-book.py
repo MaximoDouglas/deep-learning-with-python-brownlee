a8b7545bb141c1dbea6e4def2e6d94f0e41169d4
MaximoDouglas <dhms@gmail.com>
2019-01-08 16:03:54 -0300

    Binary classifier for sonar returns

---
+++ code/local_env/013_bc_sonar-book.py
@@ -0,0 +1,53 @@
+# Accuracy: 80.78%
+# Standard deviation: 5.35%
+
+# Binary Classification with Sonar Dataset: Baseline
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+# ----- Begin data preprocessing
+
+# load dataset
+dataframe = pandas.read_csv("./data/sonar.data", header=None)
+dataset = dataframe.values
+
+# split into input (X) and output (Y) variables
+X = dataset[:,0:60].astype(float)
+Y = dataset[:,60]
+
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+
+# ----- End data preprocessing
+
+# baseline model
+def create_baseline():
+  # create model
+  model = Sequential()
+  model.add(Dense(60, input_dim=60, kernel_initializer='normal', activation='relu'))
+  model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+  # Compile model
+  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  return model
+
+# evaluate model with standardized dataset
+estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5)
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, encoded_Y, cv=skfold)
+
+# Summarize
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))

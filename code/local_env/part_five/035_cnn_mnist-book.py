15e776e31cc523e6036d0b0359997f7345887ca9
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-01-13 10:12:41 -0300

    Simple CNN on MNIST data

---
+++ code/local_env/part_five/035_cnn_mnist-book.py
@@ -0,0 +1,57 @@
+# Simple CNN for the MNIST Dataset
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# reshape to be [samples][channels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
+
+# normalize inputs from 0-255 to 0-1
+X_train = X_train/255
+X_test = X_test/255
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+# define a simple CNN model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), padding='valid', input_shape=(28, 28, 1),
+    activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# build the model
+model = baseline_model()
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200,
+verbose=2)
+
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test)
+print("CNN Error: %.2f%%" % (100-scores[1]*100))

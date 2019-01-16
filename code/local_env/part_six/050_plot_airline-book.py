c198b077922d9242155c282cbffe9931b17698b0
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-01-16 06:40:47 -0300

    Load and plot airline data

---
+++ code/local_env/part_six/050_plot_airline-book.py
@@ -0,0 +1,6 @@
+import pandas
+import matplotlib.pyplot as plt
+dataset = pandas.read_csv('../data/international-airline-passengers.csv', usecols=[1], 
+                          engine='python', skipfooter=3)
+plt.plot(dataset)
+plt.show()
